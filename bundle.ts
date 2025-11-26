14a57ddfc507a221b285e5e8438b5f29d62ed845
Lelouch He <lelouchhe@gmail.com>
2025-11-25 20:32:07 -0800

    Update bundle.ts

--- bundle.ts
+++ bundle.ts
@@ -1,7 +1,7 @@
 export * from "chart.js/auto";
 
-import Chart, { Colors } from "chart.js/auto";
+import Chart from "chart.js/auto";
 import annotation from "chartjs-plugin-annotation";
 
-Chart.register(Colors);
 Chart.register(annotation);
+
